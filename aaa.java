1ae2de0277107d4d832473ff0b9249c772cbb964
ChiangMingChung <114204221+ChiangMingChung@users.noreply.github.com>
2022-12-23 11:47:41 +0800

    Add files via upload

---
+++ aaa.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class  aaa{
+	public static void main(String[] args){
+		int[] myArr = {7,5,4,9,8,1,2};
+		int a=0;
+		for(int i=0;i<3;i++ ){
+			a=myArr[i]+myArr[i+1];
+			System.out.printf("number%1.f%n",a/2);
+		}
+		System.out.printf("number%d%n",myArr[3]);
+	}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+}
